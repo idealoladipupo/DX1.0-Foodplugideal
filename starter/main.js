6de2a5b12675f089602a9e100d7718de6286a5f3
idowu oladipupo <oladipupo53@gmail.com>
2023-08-03 05:21:05 -0700

    implement hamburger functionality

--- starter/main.js
+++ starter/main.js
@@ -1,6 +1,12 @@
 var hamburger = document.querySelector(".navmobile-menu")
+var mobileLinksHolder = document.querySelector('.navmobile-links')
+var backdrop = document.querySelector('.backdrop')
+
 
 function hamburgerHandler(){
     hamburger.classList.toggle("showMenu")
+    mobileLinksHolder.classList.toggle('Shownnavmobile-Links')
+    backdrop.classList.toggle('showbackdrop')
+
 
 }
